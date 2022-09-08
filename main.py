eea077f308bfad24ae4fe67971c645d9383cd504
Mateusz Gawronkiewicz <gawmath007@gmail.com>
2022-09-08 20:12:37 +0200

    Zbieranie pozycji kursora

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from ctypes import windll, Structure, c_long, byref
+import time
+
 
 
 class MojeWidgety(Widget):
+    #def on_touch_move(self, touch):
     pass
+
+
+
 
 class MyryProgram(App):
     pass
 
+
+class Cursor():
+    now = time.time()
+    future = now + 10000
+    while time.time() < future:
+        class POINT(Structure):
+            _fields_ = [("x", c_long), ("y", c_long)]
+
+
+        def queryMousePosition():
+            pt = POINT()
+            windll.user32.GetCursorPos(byref(pt))
+            return {"x": pt.x, "y": pt.y}
+
+
+        pos = queryMousePosition()
+        print(pos)
 
 MyryProgram().run()
