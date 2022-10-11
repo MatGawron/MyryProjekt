ca27864e908e5aba6e61b4164c8ce832b96be693
myrychyry <mrcharlik1999@gmail.com>
2022-10-11 15:33:17 +0200

    Gzuiek wie kiedy jest wcisniety

--- main.py
+++ main.py
@@ -12,41 +12,5 @@ class InterfaceApp(App):
     def build(self):
         return Interface()
 
-
-class MojeWidgety(Widget):
-    def on_touch_down(self, touch):
-        touch.grab(self)
-        posX = queryMousePositionX()
-        posY = queryMousePositionY()
-        print(posY, posX)
-
-
-
-
-class MyryProgram(App):
-    pass
-
-
-
-class POINT(Structure):
-    _fields_ = [("x", c_long), ("y", c_long)]
-
-
-def queryMousePositionX():
-    pt = POINT()
-    windll.user32.GetCursorPos(byref(pt))
-    return {pt.x}
-
-def queryMousePositionY():
-    pt = POINT()
-    windll.user32.GetCursorPos(byref(pt))
-    return {pt.y}
-
-
-
-
 InterfaceApp().run()
-MyryProgram().run()
-#from kivy.app import App
-#from kivy.uix.widget import Widget
 
