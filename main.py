4aa8c5e619e391bb8c70467470d3e14c89a449f7
myrychyry <mrcharlik1999@gmail.com>
2022-10-12 09:26:39 +0200

    Lethoskrucone

--- main.py
+++ main.py
@@ -10,11 +10,23 @@ class GuzikWyglad(Button):
 class Interface(Widget):
     def odklikujka_Guzika(self):
         self.ids.naszguzik.selected = False
+        self.ids.naszguzik2.selected = False
+        self.ids.naszguzik3.selected = False
     def guzik_click(self):
         nastpepny_stan = not self.ids.naszguzik.selected
         self.odklikujka_Guzika()
         self.ids.naszguzik.selected = nastpepny_stan
         print("gUZIK  W k l i k n i e n t y")
+    def guzik_click(self):
+        nastpepny_stan = not self.ids.naszguzik2.selected
+        self.odklikujka_Guzika()
+        self.ids.naszguzik2.selected = nastpepny_stan
+        print("guz2  W k l i k n i e n t y")
+    def guzik_click(self):
+        nastpepny_stan = not self.ids.naszguzik3.selected
+        self.odklikujka_Guzika()
+        self.ids.naszguzik3.selected = nastpepny_stan
+        print("guz3  W k l i k n i e n t y")
 
 class InterfaceApp(App):
 
